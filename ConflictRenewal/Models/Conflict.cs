11e8659a6e2c7197e81ced27b81ee92da775f205
Jimmy McW <jimmy.mcw@live.com>
2018-11-03 09:13:53 +0800

    Add required fields.

--- ConflictRenewal/Models/Conflict.cs
+++ ConflictRenewal/Models/Conflict.cs
@@ -16,26 +16,32 @@ namespace ConflictRenewal.Models
 
         [Display(Name = "Conflict Trigger")]
         [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Response to question 1 required.")]
         public string Question1 { get; set; }
 
         [Display(Name = "Emotional Experience")]
         [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Response to question 2 required.")]
         public string Question2 { get; set; }
 
         [Display(Name = "Conflict Statement")]
         [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Response to question 3 required.")]
         public string Question3 { get; set; }
 
         [Display(Name = "Desired Outcome")]
         [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Response to question 4 required.")]
         public string Question4 { get; set; }
 
         [Display(Name = "Solution Rehearsal")]
         [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Response to question 5 required.")]
         public string Question5 { get; set; }
 
         [Display(Name = "Permanent Solution")]
         [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Response to question 6 required.")]
         public string Question6 { get; set; }
 
         [Display(Name = "Most recent journal")]
